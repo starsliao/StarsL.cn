7683472fce4595b66253512a487bb3f5f02c2e2d
starsliao <starsliao@163.com>
2020-01-12 15:05:44 +0800

    mod news

--- TOOLS/getcnbate.py
+++ TOOLS/getcnbate.py
@@ -9,13 +9,14 @@ d = feedparser.parse('https://rss.cnbeta.com/')
 now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 conn=MySQLdb.connect(host='127.0.0.1',user='starsliao',passwd='wefw$jh37@4S',db='starsl.cn',port=3306, charset='utf8mb4')
 cur=conn.cursor()
-for i in range(14):
+for i in range(15):
     num = random.randint(0,3)
     txt = d['entries'][i]['title']
     url = d['entries'][i]['id']
-    insert_sql = f"INSERT INTO `starsl.cn`.blog_news(title, summary, news_from, url, create_time, pub_time) VALUES ('{txt}', '', {num}, '{url}', '{now}', '{now}')"
+    if 'cnbeta.com' in url:
+        insert_sql = f"INSERT INTO `starsl.cn`.blog_news(title, summary, news_from, url, create_time, pub_time) VALUES ('{txt}', '', {num}, '{url}', '{now}', '{now}')"
 #    print(insert_sql)
-    cur.execute(insert_sql)
+        cur.execute(insert_sql)
 conn.commit()
 cur.close()
 conn.close()
